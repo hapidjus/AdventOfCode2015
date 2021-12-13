782645db7bfcd5a4bcd186b980c3975d90bbf743
hapidjus <5553121+hapidjus@users.noreply.github.com>
2021-12-13 21:02:28 +0100

    Add 4a and 4b

---
+++ 4a.php
@@ -0,0 +1,18 @@
+<?php
+include 'helpers.php';
+$input = 'yzbqklnj';
+$iterations = PHP_INT_MAX;
+
+for ($i = 0; $i <= $iterations; $i++) {
+    $hash = md5($input . $i);
+    if (
+        $hash[0] === '0'
+        && $hash[1] === '0'
+        && $hash[2] === '0'
+        && $hash[3] === '0'
+        && $hash[4] === '0'
+    ) {
+        echo $hash . PE;
+        echo $i . PE;
+    }
+}
