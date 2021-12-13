4df0e89b0db6bfdeb271fc6752a0ecc2d117e2fa
hapidjus <5553121+hapidjus@users.noreply.github.com>
2021-12-13 21:02:46 +0100

    Create helpers.php

---
+++ helpers.php
@@ -0,0 +1,33 @@
+<?php
+
+const PE = PHP_EOL;
+const BR = '<br>';
+
+function dp($input)
+{
+    echo '<pre>';
+    var_dump($input);
+    echo '</pre>';
+    die();
+}
+
+function dump($input)
+{
+    var_dump($input);
+}
+
+function dd($input)
+{
+    var_dump($input);
+    die();
+}
+
+function pause()
+{
+    $handle = fopen("php://stdin", "r");
+    do {
+        $line = fgets($handle);
+    } while ($line == '');
+    fclose($handle);
+    return $line;
+}
